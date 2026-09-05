b27b50f5319c8b8a87e9cc55c490196abe52bcc8
shoonia <alexanderz@wix.com>
2026-09-05 14:17:43 +0300

    feat: enhance if statement tests with block statements and ensure else-if conditions are preserved

--- tests/if_tatement.rs
+++ tests/if_tatement.rs
@@ -25,15 +25,119 @@ fn remove_if() {
 #[test]
 fn should_be_block_stmt_alternate() {
     run_test(
-        "if (!Date.now) console.log(1); else { console.log(2); console.log(3); }",
-        "{ console.log(2); console.log(3); }",
+        r#"
+        if (!Date.now) 
+          console.log(1);
+        else {
+          console.log(2);
+          console.log(3);
+        }
+        "#,
+        r#"
+        {
+          console.log(2);
+          console.log(3);
+        }
+        "#,
     );
 }
 
 #[test]
 fn should_be_block_stmt_consequent() {
     run_test(
-        "if (Date.now) { console.log(1); console.log(2); } else console.log(3);",
-        "{ console.log(1); console.log(2); }",
+        r#"
+        if (Date.now) {
+          console.log(1);
+          console.log(2);
+        } else
+          console.log(3);
+        "#,
+        r#"
+        {
+          console.log(1);
+          console.log(2);
+        }
+        "#,
+    );
+}
+
+#[test]
+fn should_not_broken_else_if() {
+    run_test(
+        r#"
+      if (!Date.now) {
+        console.log(1);
+        console.log(2);
+      } else if (aCondition) {
+        console.log(3);
+        console.log(4);
+      } else {
+        console.log(5);
+        console.log(6);
+      }
+    "#,
+        r#"
+      if (aCondition) {
+        console.log(3);
+        console.log(4);
+      } else {
+        console.log(5);
+        console.log(6);
+      }
+      "#,
+    );
+}
+
+#[test]
+fn should_not_broken_else_if_2() {
+    run_test(
+        r#"
+      if (aCondition) {
+        console.log(1);
+        console.log(2);
+      } else if (Date.now) {
+        console.log(3);
+        console.log(4);
+      } else {
+        console.log(5);
+        console.log(6);
+      }
+    "#,
+        r#"
+      if (aCondition) {
+        console.log(1);
+        console.log(2);
+      } else {
+        console.log(3);
+        console.log(4);
+      }
+      "#,
+    );
+}
+
+#[test]
+fn should_not_broken_else_if_3() {
+    run_test(
+        r#"
+      if (aCondition) {
+        console.log(1);
+        console.log(2);
+      } else if (!Date.now) {
+        console.log(3);
+        console.log(4);
+      } else {
+        console.log(5);
+        console.log(6);
+      }
+    "#,
+        r#"
+      if (aCondition) {
+        console.log(1);
+        console.log(2);
+      } else {
+        console.log(5);
+        console.log(6);
+      }
+      "#,
     );
 }
